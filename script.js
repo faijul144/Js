99e3139ca3816e827ce62bd4ae5396a3d0b162d7
faijul144 <f.faijulislam144@gmail.com>
2019-03-18 19:47:57 +0600

    Date JS Added

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert('Yahoo Uploaded To Git');
+var today = new Date();
+var container = document.getElementById("container");
+var ul = document.createElement('ul');
+ul.classList.add('list-unstyled');
+var liDate = document.createElement('li');
+liDate.innerHTML = today.getDate();
+ul.appendChild(liDate);
+var liYear = document.createElement('li');
+liYear.innerHTML = today.getFullYear();
+ul.appendChild(liYear);
+var liMonth = document.createElement('li');
+liMonth.innerHTML = today.getMonth();
+ul.appendChild(liMonth);
+var liDay = document.createElement('li');
+liDay.innerHTML = today.getDay();
+ul.appendChild(liDay);
+var liHour = document.createElement('li');
+liHour.innerHTML = today.getHours();
+ul.appendChild(liHour);
+var liMin = document.createElement('li');
+liMin.innerHTML = today.getMinutes();
+ul.appendChild(liMin);
+var liSec = document.createElement('li');
+ul.appendChild(liSec);
+liSec.innerHTML = today.getSeconds();
+container.appendChild(ul);
+
