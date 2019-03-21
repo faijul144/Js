19b28f9d3632102e34948c4768df77a87e4884b7
faijul144 <f.faijulislam144@gmail.com>
2019-03-21 13:52:50 +0600

    Completed Book List

--- book.js
+++ book.js
@@ -4,31 +4,57 @@ this.auth = auth;
 this.isbn = isbn;
 }
 
+
+const form = document.getElementById('book-form');
+const container = document.getElementById('container');
+const list = document.getElementById('book_list');
+const row = document.createElement('tr');
 // function for UI
 function UI(){}
 
+UI.prototype.deleteBook = function(target) {
+    if(target.parentElement.className === 'delete'){
+        target.parentElement.parentElement.parentElement.remove();
+    }
+}
 
 UI.prototype.addNewBook = function(book) {
-    const list = document.getElementById('book_list');
 
-    const row = document.createElement('tr');
 
     row.innerHTML = `
     <td>${book.title}</td>
     <td>${book.auth}</td>
     <td>${book.isbn}</td>
-    <td><a href="javascript:void(0)"><i <i class="fas fa-times-circle "></i></a></td>
+    <td><a href="javascript:void(0)" class="delete"><i class="fas fa-times-circle"></i></a></td>
     `;
     list.appendChild(row);
 
-    document.getElementById('title').value = " ";
-    document.getElementById('auth').value = " ";
-    document.getElementById('isbn').value = " ";
+    document.getElementById('title').value = "";
+    document.getElementById('auth').value = "";
+    document.getElementById('isbn').value = "";
 }
 
 
+UI.prototype.alertMessageEmpty = function() {
+
+    const err = document.createElement('div');
+
+    err.classList.add('alert', 'alert-danger', 'alert-dismissible', 'fade' ,'show');
+
+    err.innerHTML = `
+    <strong>Opps You Missed Something!!</strong>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+    `;
+    container.insertBefore(err,form);
+
+}
+
+
+
 // Event Listener
-document.getElementById('book-form').addEventListener('submit',function(e){
+form.addEventListener('submit',function(e){
 
     const title = document.getElementById('title').value,
           auth = document.getElementById('auth').value,
@@ -38,7 +64,22 @@ document.getElementById('book-form').addEventListener('submit',function(e){
     // console.log(book);
 
     const ui = new UI();
+
+    if(title === "" || auth === "" || isbn === ""){
+        ui.alertMessageEmpty();
+    }else{
     ui.addNewBook(book);
+}
+    e.preventDefault();
+})
+
+// Delete Listener
+
+list.addEventListener('click',function(e){
+
+    const ui = new UI();
+    ui.deleteBook(e.target);
+    console.log(e.target.parentElement.className);
 
     e.preventDefault();
 })
